f3db575a0bc6bd6485a6b8bd005146b5d65ffca2
jasmin <jasmindudekula4@gmail.com>
2025-10-11 15:23:08 +0530

    Role

---
+++ src/controllers/Role.controller.ts
@@ -0,0 +1,60 @@
+import * as z from "zod";
+import TryCatch from "../utils/TryCatch.js";
+import ApiError from "../Error/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import { role } from "../schemas/role.schema.js";
+import Roles from "../models/roles.js";
+
+class RoleController{
+     createRole=TryCatch( async(req, res) => {
+    const parsed=role.parse(req.body);
+    const existingRole=await Roles.findOne({roleName: parsed.roleName});
+    if (existingRole) {
+      throw new ApiError("Role name already exists", 409, false);
+    }
+    const newRole=await Roles.create(parsed);
+    res.json(new ApiResponse("Role created successfully",201, newRole));
+  });
+
+
+    getAllRoles=TryCatch( async(req, res)=>{
+    const roles=await Roles.find();
+    res.json(new ApiResponse("Roles fetched successfully", 200, roles));
+    throw new ApiError("this is for example", 401, false);
+  });
+
+   
+  getRoleById=TryCatch( async(req,res) => {
+    const {id}=req.params;
+    const roleData= await Roles.findById(id);
+    if (!roleData) {
+      throw new ApiError("Role not found",404,false);
+    }
+    res.json(new ApiResponse("Role fetched successfully",200,roleData));
+  });
+
+  updateRole=TryCatch(async (req, res) => {
+    const {id}=req.params;
+    const parsed = role.partial().parse(req.body); 
+    const updatedRole = await Roles.findByIdAndUpdate(id, parsed, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedRole) {
+      throw new ApiError("Role not found", 404, false);
+    }
+    res.json(new ApiResponse("Role updated successfully", 200, updatedRole));
+  });
+
+  deleteRole=TryCatch( async(req, res) => {
+    const { id } = req.params;
+    const deletedRole = await Roles.findByIdAndDelete(id);
+    if (!deletedRole) {
+      throw new ApiError("Role not found", 404, false);
+    }
+    res.json(new ApiResponse("Role deleted successfully", 200, deletedRole));
+  });
+
+}
+
+export default RoleController;
